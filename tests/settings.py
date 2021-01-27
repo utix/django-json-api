a53f85fdbc5aec83c4cad3897b1b0f87a7c87512
Victor Sprengel <victor.sprengel@gmail.com>
2021-01-27 18:16:23 -0300

    Fix settings.py not using env vars

--- tests/settings.py
+++ tests/settings.py
@@ -10,17 +10,17 @@ INSTALLED_APPS = [
 
 SERVICE = 'test'
 ENVIRONMENT = 'test'
-SECRET_KEY = 'tagada-tsoin-tsoin-137'
+SECRET_KEY = 'rrrrrr-rrrrr-rrrrr-rrr'
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'psqlextra.backend',
-        'NAME': 'postgres',
-        'USER': 'vic',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
         'TEST': {
             'NAME': 'auto_tests',
         },
