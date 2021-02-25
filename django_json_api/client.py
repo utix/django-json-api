0a925a0e32f49b01d16722a5effb9a47fad47d46
Victor Sprengel <victor.sprengel@gmail.com>
2021-02-25 16:01:39 -0300

    Add default User-Agent in JSONAPIC'ient headers

--- django_json_api/client.py
+++ django_json_api/client.py
@@ -4,6 +4,7 @@ from urllib.parse import urlencode
 import requests
 from django.conf import settings
 
+from django_json_api import __version__
 from django_json_api.fields import Relationship, get_model
 
 Fields = Dict[str, List[str]]
@@ -26,6 +27,7 @@ class JSONAPIClient:
             {
                 "Content-Type": "application/vnd.api+json",
                 "Accept": "application/vnd.api+json",
+                "User-Agent": f"JSONAPIClient/{__version__}",
                 **getattr(settings, "DJANGO_JSON_API_ADDITIONAL_HEADERS", {}),
             }
         )
