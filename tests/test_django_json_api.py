cb7fe5c569aa7075032d8c74e9d96dff3844c3ad
Victor Sprengel <victor.sprengel@gmail.com>
2021-02-26 12:28:03 -0300

    Bump version to v0.1.1

--- tests/test_django_json_api.py
+++ tests/test_django_json_api.py
@@ -2,4 +2,4 @@ from django_json_api import __version__ as django_json_api_version
 
 
 def test_version() -> None:
-    assert django_json_api_version == "0.1.0"
+    assert django_json_api_version == "0.1.1"
